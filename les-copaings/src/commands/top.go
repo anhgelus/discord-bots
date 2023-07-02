b04b134b8455f2c3d9899e76bb74591e1437c9f9
Anhgelus Morhtuuzh <anhgelus.morhtuuzh@gmx.com>
2023-07-03 00:43:24 +0200

    feat(command): better top

--- les-copaings/src/commands/top.go
+++ les-copaings/src/commands/top.go
@@ -6,6 +6,7 @@ import (
 	"github.com/anhgelus/discord-bots/les-copaings/src/utils"
 	"github.com/anhgelus/discord-bots/les-copaings/src/xp"
 	"github.com/bwmarrin/discordgo"
+	"time"
 )
 
 func Top(client *discordgo.Session, i *discordgo.InteractionCreate) {
@@ -18,9 +19,25 @@ func Top(client *discordgo.Session, i *discordgo.InteractionCreate) {
 			utils.SendAlert(err.Error())
 			return
 		}
-		msg += fmt.Sprintf("%d. %s - niveau : %d\n", i+1, user.Username, xp.CalcLevel(top.XP))
+		msg += fmt.Sprintf("%d. **%s** - niveau : %d\n", i+1, user.Username, xp.CalcLevel(top.XP))
 	}
-	err := respondInteraction(client, i, msg)
+	err := client.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
+		Type: discordgo.InteractionResponseChannelMessageWithSource,
+		Data: &discordgo.InteractionResponseData{
+			Embeds: []*discordgo.MessageEmbed{
+				{
+					Title:       "Top",
+					Description: "Les membres les plus actifs du serveur !\n" + msg,
+					Author:      &discordgo.MessageEmbedAuthor{Name: i.Member.User.Username},
+					Footer: &discordgo.MessageEmbedFooter{
+						Text: "© 2023 - Les Copaings",
+					},
+					Color:     0x10E6AD,
+					Timestamp: time.Now().Format(time.RFC3339),
+				},
+			},
+		},
+	})
 	if err != nil {
 		utils.SendAlert(err.Error())
 	}
