fbb5f649c198b7239c53457abf68dd857a4d28e9
Léo-21 <leo.kos.mars@gmail.com>
2023-07-08 16:36:09 +0200

    feat(commands): ping command actually sends ping

--- les-copaings/src/utils/discordgo.go
+++ les-copaings/src/utils/discordgo.go
@@ -2,6 +2,8 @@ package utils
 
 import (
 	"github.com/bwmarrin/discordgo"
+	"strconv"
+	"time"
 )
 
 func FetchGuildUser(s *discordgo.Session, guildID string) []*discordgo.Member {
@@ -10,4 +12,16 @@ func FetchGuildUser(s *discordgo.Session, guildID string) []*discordgo.Member {
 		SendAlert("discordgo.go - Failed to fetch guild users", err.Error())
 	}
 	return member
+}
+
+func GetTimestampFromId(id string) (time.Time, error) {
+	idInt, err := strconv.ParseInt(id, 10, 64)
+	if err != nil {
+		return time.UnixMilli(0), err
+	}
+
+	// https://discord.com/developers/docs/reference#snowflakes-snowflake-id-format-structure-left-to-right
+	timestamp := (idInt >> 22) + 1420070400000
+
+	return time.UnixMilli(timestamp), nil
 }
