1926a25c4da66f500e8e593baa80f098bb557d1a
Anhgelus Morhtuuzh <anhgelus.morhtuuzh@proton.me>
2023-08-10 15:05:41 +0200

    fix(les-copaings xp): error nil on interaction

--- les-copaings/src/init/command_handlers.go
+++ les-copaings/src/init/command_handlers.go
@@ -16,7 +16,7 @@ func CommandHandlers(s *discordgo.Session) {
 	}
 	s.AddHandler(func(s *discordgo.Session, i *discordgo.InteractionCreate) {
 		if h, ok := commandsHandler[i.ApplicationCommandData().Name]; ok {
-			copaing := sql.GetCopaing(i.User.ID, i.GuildID)
+			copaing := sql.GetCopaing(i.Member.User.ID, i.GuildID)
 			if xp.NewXp(i.Member, &copaing, 0) {
 				xp.UpdateRolesNoMessage(&copaing, s)
 			}
