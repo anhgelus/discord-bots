664fa057ddcc02b104b624ba93af6f19cf636d62
Anhgelus Morhtuuzh <anhgelus.morhtuuzh@proton.me>
2023-08-10 15:01:19 +0200

    feat(les-copaings xp): update xp on interaction

--- les-copaings/src/init/command_handlers.go
+++ les-copaings/src/init/command_handlers.go
@@ -1,6 +1,8 @@
 package start
 
 import (
+	"github.com/anhgelus/discord-bots/les-copaings/src/db/sql"
+	"github.com/anhgelus/discord-bots/les-copaings/src/xp"
 	"github.com/bwmarrin/discordgo"
 )
 
@@ -14,11 +16,11 @@ func CommandHandlers(s *discordgo.Session) {
 	}
 	s.AddHandler(func(s *discordgo.Session, i *discordgo.InteractionCreate) {
 		if h, ok := commandsHandler[i.ApplicationCommandData().Name]; ok {
-			//copaing := sql.GetCopaing(i.User.ID, i.GuildID)
-			//if xp.NewXp(i.Member, &copaing, 0) {
-			//	xp.UpdateRolesNoMessage(&copaing, s)
-			//}
-			//sql.Save(&copaing)
+			copaing := sql.GetCopaing(i.User.ID, i.GuildID)
+			if xp.NewXp(i.Member, &copaing, 0) {
+				xp.UpdateRolesNoMessage(&copaing, s)
+			}
+			sql.Save(&copaing)
 			h(s, i)
 		}
 	})
