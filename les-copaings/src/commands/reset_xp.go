7ce980c658f8ffaa41ce656e8227dc1cc84b72c5
Anhgelus Morhtuuzh <anhgelus.morhtuuzh@proton.me>
2023-08-10 15:50:42 +0200

    fix(les-copaings command): not pointer for reset_xp

--- les-copaings/src/commands/reset_xp.go
+++ les-copaings/src/commands/reset_xp.go
@@ -7,7 +7,7 @@ import (
 )
 
 func ResetXP(client *discordgo.Session, i *discordgo.InteractionCreate) {
-	sql.DB.Where("guild_id = ?", i.GuildID).Delete(sql.Copaing{})
+	sql.DB.Where("guild_id = ?", i.GuildID).Delete(&sql.Copaing{})
 	err := respondEphemeralInteraction(client, i, "XP Reset")
 	if err != nil {
 		utils.SendAlert("reset_xp.go - Interaction respond", err.Error())
