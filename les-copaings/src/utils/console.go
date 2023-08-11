c5eaf8611a4f4725cf1d4b532f3f54123aad3e68
Anhgelus Morhtuuzh <anhgelus.morhtuuzh@proton.me>
2023-08-11 19:44:45 +0200

    doc(les-copaings log): better doc

--- les-copaings/src/utils/console.go
+++ les-copaings/src/utils/console.go
@@ -8,44 +8,27 @@ import (
 
 var reset = "\033[0m"
 
-/*
-SendSuccess
-Send input strings as ANSI color code for console
-As success the color is green.
-*/
+// SendSuccess sends a success message
 func SendSuccess(message string) {
 	log.Default().Println(logger.Green, message, reset)
 }
 
-/*
-SendWarn
-Send input strings as ANSI color code for console
-As warning the color is yellow.
-*/
+// SendWarn sends a warning message
 func SendWarn(message string) {
 	log.Default().Println(logger.Yellow, message, reset)
 }
 
-/*
-SendDebug
-Send input strings as ANSI color code for console
-As debug the color is ??.
-*/
+// SendDebug sends a debug message
 func SendDebug(message ...any) {
 	log.Default().Println(logger.Cyan, message, reset)
 }
 
-/*
-SendAlert
-Send input strings as ANSI color code for console
-As warning the color is red.
-*/
+// SendAlert sends an alert
 func SendAlert(pos string, message string) {
-	color := "\033[31m" // red color in ANSI
-	fmt.Printf("[%s] %s%s%s\n", pos, color, message, reset)
-	log.Default().Println(logger.Red, fmt.Sprintf("[%s] %s%s%s", pos, color, message, reset))
+	log.Default().Println(fmt.Sprintf("[%s] %s%s%s", pos, logger.Red, message, reset))
 }
 
+// SendError sends an error (like a panic(any...))
 func SendError(err error) {
 	log.Default().Panic(err)
 }
